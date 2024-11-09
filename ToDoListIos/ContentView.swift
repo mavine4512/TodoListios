0d9c1a0c5e25274bc92a7f39660e9901aa7b941d
mavine4512 <mavineoyugi@gmail.com>
2024-11-09 12:06:33 +0300

    Initial Commit

---
+++ ToDoListIos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoListIos
+//
+//  Created by Mavine Naaman on 09/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDoListIos/ToDoListIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListIosApp.swift
+//  ToDoListIos
+//
+//  Created by Mavine Naaman on 09/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
